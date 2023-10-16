48cce4304f61080c7c46e7c153ecf6031c6a992e
trojak <mato.trojak@gmail.com>
2023-10-16 14:23:52 +0200

    Implement support for Azure vision

---
+++ libs/services/azure_vision.py
@@ -0,0 +1,43 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
+
+
+class GoogleVision:
+    def __init__(self, azure_credentials):
+        # Authenticate with Google Cloud using the key file
+        credentials = CognitiveServicesCredentials(azure_credentials['subscription_key'])
+        self.client = ComputerVisionClient(endpoint=azure_credentials['endpoint'], credentials=credentials)
+
+    def annotate_image(self, image):
+        image_path = "temp_image.png"
+        image.save(image_path, "PNG")
+        
+        with open(image_path, 'rb') as image_stream:
+            # Use the stream to call the API
+            rawHttpResponse = self.client.read_in_stream(image_stream, language="en", raw=True)
+            # try lower DPI if Bad request (azure.cognitiveservices.vision.computervision.models._models_py3.ComputerVisionOcrErrorException)
+
+        # Get ID from returned headers
+        operationLocation = rawHttpResponse.headers["Operation-Location"]
+        idLocation = len(operationLocation) - 36
+        operationId = operationLocation[idLocation:]
+        
+        # SDK call
+        result = self.client.get_read_result(operationId)
+
+        while result.status != OperationStatusCodes.succeeded and result.status != OperationStatusCodes.failed:
+            time.sleep(1)  # To avoid making too many requests in a short time
+            result = self.client.get_read_result(operationId)
+
+        return result
+
+    def process_output(self, outputs):
+        identified = []
+        if outputs.status == OperationStatusCodes.succeeded:
+            for line in outputs.analyze_result.read_results[0].lines:
+                for word in line.words:
+                    vertices = word.bounding_box
+                    identified.append({'content': word.text, 'coords': vertices[0:2] + vertices[4:6]})
+        return identified
