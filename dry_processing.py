669a5c3f2ba952eb547fc70c3651ab642fc51755
Matej Trojak <trojak@mail.muni.cz>
2023-11-07 10:06:33 +0100

    Propagate OCR results to the top level function

--- dry_processing.py
+++ dry_processing.py
@@ -61,7 +61,7 @@ def main(scanned_logsheet, template, config_file, output_file, test_set, debug):
     if debug:
         annotate_pdfs(identified_content, logsheet_image)
     # process contents
-    contents = process_content(identified_content, logsheet_image, config)
+    contents, artefacts = process_content(identified_content, logsheet_image, config)
     # store to Excel sheet
     store_results(contents, output_file)
 
