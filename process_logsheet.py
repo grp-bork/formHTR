f9da7c0d46147b1914d41aeb9578861b2263d42a
trojak <mato.trojak@gmail.com>
2023-11-09 11:46:22 +0100

    Include artefacts in process_logsheets

--- process_logsheet.py
+++ process_logsheet.py
@@ -50,9 +50,9 @@ def main(scanned_logsheet, template, config_file, output_file, google_credential
     if debug:
         annotate_pdfs(identified_content, logsheet_image)
     # process contents
-    contents = process_content(identified_content, logsheet_image, config)
+    contents, artefacts = process_content(identified_content, logsheet_image, config)
     # store to Excel sheet
-    store_results(contents, output_file)
+    store_results(contents, artefacts, output_file)
 
 
 if __name__ == '__main__':
