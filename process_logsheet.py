ce7a5441525994abdd151b7463a8b3f5f50f30c6
Matej Trojak <trojak@mail.muni.cz>
2023-11-13 14:51:10 +0100

    Implement backside option and join both results

--- process_logsheet.py
+++ process_logsheet.py
@@ -20,12 +20,12 @@ def load_credentials(google_credentials, amazon_credentials, azure_credentials):
 
     return {'google': google_credentials, 'amazon': amazon_credentials, 'azure': azure_credentials}
 
-def preprocess_input(scanned_logsheet, template, config):
+def preprocess_input(scanned_logsheet, template, config, page):
     # convert pdfs to images
     template_image = convert_pdf_to_image(template)
     template_image = np.array(template_image)
 
-    logsheet_image = convert_pdf_to_image(scanned_logsheet)
+    logsheet_image = convert_pdf_to_image(scanned_logsheet, page)
     logsheet_image = np.array(logsheet_image)
 
     # resize images
@@ -37,20 +37,42 @@ def preprocess_input(scanned_logsheet, template, config):
     return logsheet_image
 
 
-def main(scanned_logsheet, template, config_file, output_file, google_credentials, amazon_credentials, azure_credentials, debug):
+def process_logsheet(logsheet, template, config_file, credentials, debug=False, front=True):
     # load CSV config
     config = LogsheetConfig([], [])
     config.import_from_json(config_file)
+
+    page = 0 if front else 1
+
     # assume PDF and CSV config correspond to each other (QR codes are not reliable anyway)
-    logsheet_image = preprocess_input(scanned_logsheet, template, config)
+    logsheet_image = preprocess_input(logsheet, template, config, page)
     # call external OCR services
-    credentials = load_credentials(google_credentials, amazon_credentials, azure_credentials)
     identified_content = call_services(logsheet_image, credentials, config)
 
     if debug:
-        annotate_pdfs(identified_content, logsheet_image)
+        annotate_pdfs(identified_content, logsheet_image, front)
+
     # process contents
-    contents, artefacts = process_content(identified_content, logsheet_image, config)
+    return process_content(identified_content, logsheet_image, config)
+
+
+def main(scanned_logsheet, template, config_file, output_file, google_credentials, amazon_credentials, azure_credentials, 
+         debug, backside, backside_template, backside_config):
+    
+    credentials = load_credentials(google_credentials, amazon_credentials, azure_credentials)
+    
+    # extract contents from the front page
+    contents, artefacts = process_logsheet(scanned_logsheet, template, config_file, credentials, debug=debug)
+
+    # extract contents from the back side (if present)
+    if backside:
+        contents_back, artefacts_back = process_logsheet(scanned_logsheet, backside_template, backside_config, credentials, debug=debug, front=False)
+
+        # join results
+        contents += contents_back
+        for key in artefacts.keys():
+            artefacts[key] = artefacts[key] + artefacts_back[key]
+
     # store to Excel sheet
     store_results(contents, artefacts, output_file)
 
@@ -72,7 +94,14 @@ if __name__ == '__main__':
     required.add_argument('--azure', type=str, required=True, help='Path to Azure vision credentials')
 
     optional.add_argument('--debug', action=argparse.BooleanOptionalAction, default=False, help='Run in debug mode - output annotated PDF files.')
+    optional.add_argument('--backside', action=argparse.BooleanOptionalAction, default=False, help='Backside page present.')
+    optional.add_argument('--backside_template', type=str, help='PDF template of the backside')
+    optional.add_argument('--backside_config', type=str, help='Path to JSON file containing config of the backside')
 
     args = args_parser.parse_args()
 
-    main(args.pdf_logsheet, args.pdf_template, args.config_file, args.output_file, args.google, args.amazon, args.azure, args.debug)
+    if args.backside and (not args.backside_template or not args.backside_config):
+        args_parser.error('The --backside argument requires --backside_template and --backside_config.')
+
+    main(args.pdf_logsheet, args.pdf_template, args.config_file, args.output_file, args.google, args.amazon, args.azure, 
+         args.debug, args.backside, args.backside_template, args.backside_config)
