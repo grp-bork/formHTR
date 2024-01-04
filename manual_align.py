5f50115ef48c7583e595b20bca4800dadf244e05
Matej Trojak <trojak@mail.muni.cz>
2024-01-04 11:20:54 +0100

    Improve interface

--- manual_align.py
+++ manual_align.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import argparse
 
-from libs.pdf_to_image import convert_pdf_to_image
-from libs.processing.align_images import compute_closest_point
+from libs.pdf_to_image import convert_pdf_to_image, resize_image
+from libs.processing.align_images import compute_closest_point, transform
 
 
 def select_points(image):
@@ -17,7 +18,7 @@ def select_points(image):
 
     while len(points) < 4:
         # TODO end only on q or Esc
-        cv2.imshow("Select Points", image)
+        cv2.imshow('Select Points', image)
         cv2.setMouseCallback('Select Points', click_event)
         key = cv2.waitKey(0)
 
@@ -34,47 +35,64 @@ def select_points(image):
     cv2.destroyAllWindows()
     return points
 
-def align_images(template, target, template_points, target_points):
-    template_points = np.float32(template_points)
-    target_points = np.float32(target_points)
+def store_output():
+    pass
 
-    matrix = cv2.getPerspectiveTransform(target_points, template_points)
-    result = cv2.warpPerspective(target, matrix, (template.shape[1], template.shape[0]))
 
-    return result
+def process(target, template):
+    height, width, _ = target.shape
 
-def main(template_path, target_path, output_path):
-    # Convert PDF images to OpenCV format
-    template = np.array(convert_pdf_to_image(template_path))
-    target = np.array(convert_pdf_to_image(target_path))
+    target = resize_image(target, (width, height))
+    template = resize_image(template, (width, height))
 
     # Display and select points on the template image
     template_points = select_points(template.copy())
-    print(template_points)
+    # template_points = [(29, 233), (2218, 236), (30, 3074), (2213, 3084)]
+
+    # sort by finding closest points to corners
+    template_points = [compute_closest_point((0, 0), template_points),
+                       compute_closest_point((width, 0), template_points),
+                       compute_closest_point((width, height), template_points),
+                       compute_closest_point((0, height), template_points)]
 
     # Display and select points on the target image
     target_points = select_points(target.copy())
-    print(target_points)
+    # target_points = [(149, 322), (2435, 338), (107, 3270), (2396, 3306)]
 
-    # TODO sort by finding closest points to corners
-    # compute_closest_point
+    target_points = [compute_closest_point((0, 0), target_points),
+                     compute_closest_point((width, 0), target_points),
+                     compute_closest_point((width, height), target_points),
+                     compute_closest_point((0, height), target_points)]
 
     # Align the images based on the selected points
-    aligned_image = align_images(template, target, template_points, target_points)
+    return transform(target, template, target_points, template_points)
+
+
+def main(template_path, target_path, output_path, backside_template):
+    # Convert PDF images to OpenCV format
+    # TODO dont touch other pages if present? (backside)
+    template = np.array(convert_pdf_to_image(template_path))
+    target = np.array(convert_pdf_to_image(target_path))
+
+    aligned_image = process(template, target)
 
     # TODO store as pdf
     # Save the aligned image to the output file
     cv2.imwrite(output_path, aligned_image)
 
-if __name__ == "__main__":
-    import sys
 
-    # TODO make proper interface
-    # dont touch other pages if present? (backside)
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser(description='Manual alignment tool for PDF files')
 
-    if len(sys.argv) != 4:
-        print("Usage: python manual_alignment_tool.py template.pdf target.pdf output.jpg")
-        sys.exit(1)
+    args_parser._action_groups.pop()
+    required = args_parser.add_argument_group('required arguments')
+    optional = args_parser.add_argument_group('optional arguments')
 
-    template_path, target_path, output_path = sys.argv[1], sys.argv[2], sys.argv[3]
-    main(template_path, target_path, output_path)
+    required.add_argument('--pdf_template', type=str, required=True, help='Path to the template PDF file')
+    required.add_argument('--pdf_logsheet', type=str, required=True, help='Path to the target PDF file')
+    required.add_argument('--output', type=str, required=True, help='Path to the output aligned image file')
+
+    optional.add_argument('--backside_template', type=str, help='PDF template of the backside')
+
+    args = args_parser.parse_args()
+    main(args.pdf_template, args.pdf_logsheet, args.output, args.backside_template)
