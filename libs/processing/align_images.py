e9631cdfe66d950bc48e19f8d788327311a55e0c
trojak <mato.trojak@gmail.com>
2024-02-09 10:50:35 +0100

    Approximate (possibly rotated) rectangle around largest contour in alignment

--- libs/processing/align_images.py
+++ libs/processing/align_images.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
-import math
+from math import dist
 
 
 def compute_closest_point(point, corners):
-    distances = [math.dist(point, corner) for corner in corners]
+    distances = [dist(point, corner) for corner in corners]
     closest_index = np.argmin(distances)
     return corners[closest_index]
 
@@ -20,30 +20,20 @@ def find_corners(image):
 
     # Find Contours
     contours, _ = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
+    largest_contour = sorted(contours, key=cv2.contourArea, reverse=True)[0]
 
-    all_corners = []
-
-    # Loop over contours and approximate the shape
-    for c in contours:
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
-        # Assuming the box has 4 sides
-        if len(approx) == 4:
-            rect = cv2.minAreaRect(approx)
-            corners = cv2.boxPoints(rect)
-            corners = [(int(x), int(y)) for (x, y) in corners]
-            all_corners += corners
+    rect = cv2.minAreaRect(largest_contour)
+    bounding_box = cv2.boxPoints(rect)
+    bounding_box = np.int0(bounding_box)
 
     # to determine image corner points
     height, width, _ = image.shape
 
     # Order of corners is top-left, top-right, bottom-right, bottom-left
-    outer_corners = [compute_closest_point((0, 0), all_corners),
-                     compute_closest_point((width, 0), all_corners),
-                     compute_closest_point((width, height), all_corners),
-                     compute_closest_point((0, height), all_corners)]
+    outer_corners = [compute_closest_point((0, 0), bounding_box),
+                     compute_closest_point((width, 0), bounding_box),
+                     compute_closest_point((width, height), bounding_box),
+                     compute_closest_point((0, height), bounding_box)]
 
     return outer_corners
 
