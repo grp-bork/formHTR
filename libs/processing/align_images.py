7f44dd4f1b9e128be9023f2e1c6e2a7e8a905fc3
trojak <mato.trojak@gmail.com>
2024-09-20 13:18:35 +0200

    Fix absolute and relative tolerances handling in alignment

--- libs/processing/align_images.py
+++ libs/processing/align_images.py
@@ -3,15 +3,15 @@ import cv2
 from math import dist, isclose
 
 
-def validate_corners(corners, height, width, tol=10):
+def validate_corners(corners, height, width, tol=20):
     top_width = dist(corners[0], corners[1])
     bottom_width = dist(corners[3], corners[2])
 
     if not isclose(top_width, bottom_width, abs_tol=width/100):
         return False
 
-    percentage_top = abs(top_width - width)/width
-    percentage_bottom = abs(bottom_width - width)/width
+    percentage_top = 1 - top_width/width
+    percentage_bottom = 1 - bottom_width/width
 
     if percentage_top > tol/100 or percentage_bottom > tol/100:
         return False
@@ -19,8 +19,11 @@ def validate_corners(corners, height, width, tol=10):
     left_height = dist(corners[0], corners[3])
     right_height = dist(corners[1], corners[2])
 
-    percentage_left = abs(left_height - height)/height
-    percentage_right = abs(right_height - height)/height
+    if not isclose(left_height, right_height, abs_tol=height/100):
+        return False
+
+    percentage_left = 1 - left_height/height
+    percentage_right = 1 - right_height/height
 
     if percentage_left > tol/100 or percentage_right > tol/100:
         return False
@@ -34,12 +37,12 @@ def compute_closest_point(point, corners):
     return corners[closest_index]
 
 
-def find_corners(image, filter_grayscale, num=10):
+def find_corners(image, filter_grayscale, num=10, gray_filter=20):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
     if filter_grayscale:
-        _, gray = cv2.threshold(gray, 20, 255, cv2.THRESH_BINARY)
+        _, gray = cv2.threshold(gray, gray_filter, 255, cv2.THRESH_BINARY)
 
     # Enhanced Edge Detection
     edged = cv2.Canny(gray, 50, 150)
@@ -58,6 +61,11 @@ def find_corners(image, filter_grayscale, num=10):
         bounding_box = cv2.boxPoints(rect)
         box_points += list(np.int0(bounding_box))
 
+    copy_image = image.copy()
+    for corner in box_points:
+        copy_image = cv2.circle(copy_image, corner, radius=15, color=(0, 0, 255), thickness=15)
+    
+
     # to determine image corner points
     height, width, _ = image.shape
 
@@ -66,10 +74,17 @@ def find_corners(image, filter_grayscale, num=10):
                      compute_closest_point((width, 0), box_points),
                      compute_closest_point((width, height), box_points),
                      compute_closest_point((0, height), box_points)]
+    
+    for corner in outer_corners:
+        copy_image = cv2.circle(copy_image, corner, radius=15, color=(255, 0, 0), thickness=15)
+    
+    cv2.imshow('image', copy_image)
+    cv2.waitKey(0)
 
     corners_valid = validate_corners(outer_corners, height, width)
     if not corners_valid and num < 50:
-        return find_corners(image, filter_grayscale, num=num+10)
+        # TODO if not valid, do not do the alignment
+        return find_corners(image, filter_grayscale, num=num+10, gray_filter=gray_filter+10)
 
     return outer_corners
 
